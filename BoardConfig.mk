2823c19956170989258a654081419791d1605509
BlackWidowRoms <71955401+blackwidowroms@users.noreply.github.com>
2021-02-24 20:18:10 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,9 @@
 DEVICE_PATH := device/samsung/j4lte
-BOARD_VENDOR := samsung
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2019-10-01
-
-# HIDL
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
-
--include vendor/samsung/j4lte/BoardConfigVendor.mk
+# TARGET_KERNEL_CONFIG := j4lte_00_defconfig
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2625634304
+# Assert
+TARGET_OTA_ASSERT_DEVICE := j4ltexx,j4ltejx,j4lteub
+# Inherit common board flags
+include device/samsung/universal7570-common/BoardConfigCommon.mk
+include vendor/samsung/j4lte/BoardConfigVendor.mk
