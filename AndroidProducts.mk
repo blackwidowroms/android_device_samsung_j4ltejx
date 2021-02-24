48b60fe6aaa3253b1cd2a70978da57dd2c7d228d
BlackWidowRoms <71955401+blackwidowroms@users.noreply.github.com>
2021-02-24 20:22:13 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_j4lte.mk
+    $(LOCAL_DIR)/havoc_j4lte.mk
+
+COMMON_LUNCH_CHOICES := \
+     havoc_j4lte-userdebug \
+     havoc_j4lte-eng \
+     havoc_j4lte-user
